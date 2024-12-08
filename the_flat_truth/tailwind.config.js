1f9f4c8511ef6d7ed2498471a3711a781c82fded
eoghan523 <eoghan523@gmail.com>
2024-12-08 17:32:27 +0000

    This commit installed several packages 1. npm create vite@latest the_flat_truth --template react      2. npm install      3. npm run build     4. npm install -D tailwindcss postcss autoprefixer   5.  npx tailwindcss init      6.  npx tailwindcss init -p (generates the postcss.config.js)   7. npm install axios .8  npm install react-router-dom

---
+++ the_flat_truth/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}", // Include files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
