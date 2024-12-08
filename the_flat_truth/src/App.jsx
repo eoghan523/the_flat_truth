9a6f679a2a01713086d8e44fd95ec340d4dc9706
eoghan523 <eoghan523@gmail.com>
2024-12-08 18:36:17 +0000

    troubleeshooting router dom. The router Dom is work installing properly. It is getting a 404 error: (Please see below) npm error code E404 npm error 404 Not Found - GET https://registry.npmjs.org/react-router-dom - Not found npm error 404 npm error 404  'react-router-dom@^6.14.0' is not in this registry. npm error 404 npm error 404 Note that you can also install from a npm error 404 tarball, folder, http url, or git url. npm error A complete log of this run can be found in: C:\Users\eogha\AppData\Local\npm-cache\_logs\2024-12-08T18_34_28_341Z-debug-0.log PS C:\Users\eogha\the_flat_truth\the_flat_truth>

--- the_flat_truth/src/App.jsx
+++ the_flat_truth/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
